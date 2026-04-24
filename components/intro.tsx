50339ca532f32810c624183466a8b326c3ed9365
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-04-24 20:38:30 +0200

    added intro interface

---
+++ components/intro.tsx
@@ -0,0 +1,27 @@
+import { Show, SignInButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
+import { LayoutDashboard } from "lucide-react";
+import Link from "next/link";
+
+export default function Intro() {
+    return (
+        <div className="w-full bg-white space-y-4 rounded-[10px] max-w-[450px] p-4">
+            <h1 className="text-4xl font-serif">Mix<span className="text-green-500">Mind</span></h1>
+            
+            <Show when={'signed-out'}>
+                <SignInButton>
+                    <Button className="w-full bg-green-500 hover:bg-green-300">Sign in now!</Button>
+                </SignInButton>
+            </Show>
+
+            <Show when={'signed-in'}>
+                <Button variant={'link'} className="w-full bg-green-500 hover:bg-green-300">
+                    <LayoutDashboard />
+                    <Link href="/">Continue to dashboard</Link>
+                </Button>
+            </Show>
+
+            <p className="text-gray-500">&copy; Tafadzwa Sibanda {new Date().getFullYear()}</p>
+        </div>
+    )
+}
