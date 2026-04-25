d9b1f061bc08ac4ca5ff0411e06ad8208efaa75f
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-04-25 13:25:23 +0200

    added link to main dashboard in brander

--- components/brander.tsx
+++ components/brander.tsx
@@ -18,6 +18,7 @@ import {
   useSidebar,
 } from "@/components/ui/sidebar"
 import { ChevronsUpDownIcon, PlusIcon } from "lucide-react"
+import Link from "next/link"
 
 export function Brander({
   brand,
@@ -37,18 +38,20 @@ export function Brander({
   return (
     <SidebarMenu>
       <SidebarMenuItem>
-        <SidebarMenuButton
-              size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-            >
-              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-green-500 text-sidebar-primary-foreground">
-                {activeBrand.logo}
-              </div>
-              <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{activeBrand.name}</span>
-              </div>
-              
-            </SidebarMenuButton>
+        <Link  href={'/dashboard'}>
+          <SidebarMenuButton
+                size="lg"
+                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+              >
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-green-500 text-sidebar-primary-foreground">
+                  {activeBrand.logo}
+                </div>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">{activeBrand.name}</span>
+                </div>
+                
+          </SidebarMenuButton>
+        </Link>
       </SidebarMenuItem>
     </SidebarMenu>
   )
