68bf5378a300ad78692e71af9d0650a2f9e4980b
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-04-25 06:46:40 +0200

    added other db migrations

--- db/schema.ts
+++ db/schema.ts
@@ -4,6 +4,16 @@ export const drafts = pgTable('drafts', {
     id: uuid().defaultRandom().primaryKey(),
     baseIdea: text().notNull(),
     details: text().notNull(),
+    response: text().notNull(),
+    userId: text().notNull(),
+    createdAt: timestamp().defaultNow()
+})
+
+export const plans = pgTable('plans', {
+    id: uuid().defaultRandom().primaryKey(),
+    draft: text().notNull(),
+    techDetails: text().notNull(),
+    response: text().notNull(),
     userId: text().notNull(),
     createdAt: timestamp().defaultNow()
 })
