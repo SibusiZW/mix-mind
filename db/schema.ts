470f17ddccf946764191fb88e7824047cf9e155e
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-04-25 09:35:41 +0200

    added `title` field too DB

--- db/schema.ts
+++ db/schema.ts
@@ -2,6 +2,7 @@ import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const drafts = pgTable('drafts', {
     id: uuid().defaultRandom().primaryKey(),
+    title: text().notNull(),
     baseIdea: text().notNull(),
     details: text().notNull(),
     response: text().notNull(),
@@ -11,6 +12,7 @@ export const drafts = pgTable('drafts', {
 
 export const plans = pgTable('plans', {
     id: uuid().defaultRandom().primaryKey(),
+    title: text().notNull(),
     draft: text().notNull(),
     techDetails: text().notNull(),
     response: text().notNull(),
